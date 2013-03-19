12b9856f2771aa3260baf05d41394fc42d3db135
Ha The Tung <tunght2@smartosc.com>
2013-03-19 14:18:14 +0700

    test log

--- src/SM/LogBundle/Controller/DefaultController.php
+++ src/SM/LogBundle/Controller/DefaultController.php
@@ -8,6 +8,10 @@ class DefaultController extends Controller
 {
     public function indexAction($name)
     {
+        /** @var $logger \Symfony\Bridge\Monolog\Logger */
+        $logger = $this->get('logger');
+        $logger->info('I just got the logger');
+        $logger->error('Here is an error');
         return $this->render('SMLogBundle:Default:index.html.twig', array('name' => $name));
     }
 }
