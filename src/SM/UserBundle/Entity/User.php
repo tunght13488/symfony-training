eaf9f0e58bf610c92c1dc74323f886a986be38c5
Ha The Tung <tunght2@smartosc.com>
2013-03-19 17:55:37 +0700

    config FOSUserBundle

---
+++ src/SM/UserBundle/Entity/User.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * SM
+ */
+namespace SM\UserBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User Entity
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected  $id;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
